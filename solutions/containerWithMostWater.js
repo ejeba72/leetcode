6d8f1d8c0e72dfbd8a6564692a524852988dd0b8
Emmanuel Eni <eejeba@gmail.com>
2023-02-02 11:55:35 +0100

    container with most water

--- solutions/containerWithMostWater.js
+++ solutions/containerWithMostWater.js
@@ -8,7 +8,16 @@ let n = height.lenth
 const triangle = [ 1, 2, 3, 4, 5, 6, 7, 8 ]
 const height = [ 1,8,6,2,5,4,8,3,7 ]
 
-console.log(height[7]);
+// console.log(height[7]);
 // console.log(height.length)
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    
+};
+
 
 module.exports = { height }
